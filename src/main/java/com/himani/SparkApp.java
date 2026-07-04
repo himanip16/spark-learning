b0029a5e9675f2405df9dd922bde05bdd3cae1fa
himani <himani44prasad@gmail.com>
2026-07-04 23:06:56 +0530

    read csv

--- src/main/java/com/himani/SparkApp.java
+++ src/main/java/com/himani/SparkApp.java
@@ -1,17 +1,33 @@
 package com.himani;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.col;
 
 public class SparkApp {
 
     public static void main(String[] args) {
 
         SparkSession spark = SparkSession.builder()
-                .appName("Hello Spark")
+                .appName("CSV Demo")
                 .master("local[*]")
                 .getOrCreate();
 
-        System.out.println("Hello Spark");
+        Dataset<Row> orders = spark.read()
+                .option("header", "true")
+                .option("inferSchema", "true")
+                .csv("data/orders.csv");
+        orders.printSchema();
+        orders.show();
+        Dataset<Row> expensiveOrders =
+                orders.filter(col("amount").gt(150));
+        expensiveOrders.printSchema();
+        expensiveOrders.show();
+        expensiveOrders.write()
+                .option("header", "true")
+                .mode("overwrite")
+                .csv("output/orders");
         try {
             Thread.sleep(300000);
         } catch (InterruptedException e) {
